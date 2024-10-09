b08580752c02955211a18c9be316a52cc1fb8e7b
Weistr <1561267515@qq.com>
2024-10-09 19:20:48 +0800

    c

---
+++ Target/GD32F303/target.h
@@ -0,0 +1,7 @@
+#ifndef TARGET_H
+#define TARGET_H
+
+
+#include "gd32f30x.h"
+
+#endif // !TARGET_H
---
+++ UserApp/Inc/task.h
@@ -0,0 +1,19 @@
+#ifndef TASK_H
+#define TASK_H
+#include "main.h"
+typedef struct 
+{
+	uint8_t Run;               //任务状态：Run/Stop
+	uint16_t TIMCount;         //定时计数器
+	uint16_t TRITime;          //重载计数器
+	void (*TaskHook) (void); //任务函数
+} TASK_COMPONENTS;       
+
+//========================================================================
+//                             外部函数和变量声明
+//========================================================================
+
+void Task_Marks_Handler_Callback(void);
+void Task_Pro_Handler_Callback(void);
+void user_main(void);
+#endif // !TASK_H
