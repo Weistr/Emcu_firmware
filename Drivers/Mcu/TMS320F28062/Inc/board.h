5fea93fb33c9655cbb1d8d0b7852c039a3f0c711
Weistr <1561267515@qq.com>
2024-12-22 08:57:53 +0800

    adc,cputimer

--- Drivers/Mcu/TMS320F28062/Inc/board.h
+++ Drivers/Mcu/TMS320F28062/Inc/board.h
@@ -7,6 +7,14 @@
 
 #ifndef BOARD_H_
 #define BOARD_H_
+#include "F2806x_Cla_typedefs.h"// F2806x CLA Type definitions
+#include "F2806x_Device.h"      // F2806x Headerfile Include File
+#include "F2806x_Examples.h"    // F2806x Examples Include File
+#include "main.h"
+/*GPIO define
+TEST_MODE_GPIO:GPIO53
+
+*/
 
 void boardInit(void);
 void boardApp(void);
--- Drivers/Mcu/TMS320F28062/board.c
+++ Drivers/Mcu/TMS320F28062/board.c
@@ -5,13 +5,65 @@
  *      Author: 86133
  */
 #include "board.h"
+uint8_t flg1 = 0,flg2 = 0;
+//20ms
 void boardApp(void)
 {
+    GpioDataRegs.GPBTOGGLE.bit.GPIO53 = 1;
+    EALLOW;
+    AdcRegs.ADCSOCFRC1.bit.SOC0 = 1;
+    AdcRegs.ADCSOCFRC1.bit.SOC1 = 1;
 
+
+    EDIS;
 }
 
 void boardInit(void)
 {
+    //
+    //Initialize System Control:
+    // PLL, WatchDog, enable Peripheral Clocks
+    // This example function is found in the F2806x_SysCtrl.c file.
+    //
+    InitSysCtrl();
 
+    //
+    //  Clear all interrupts and initialize PIE vector table:
+    // Disable CPU interrupts
+    //
+    DINT;
+
+    //
+    // Initialize the PIE control registers to their default state.
+    // The default state is all PIE interrupts disabled and flags
+    // are cleared.
+    // This function is found in the F2806x_PieCtrl.c file.
+    //
+    InitPieCtrl();
+    //
+    // Disable CPU interrupts and clear all CPU interrupt flags
+    //
+    IER = 0x0000;
+    IFR = 0x0000;
+
+    //
+    // Initialize the PIE vector table with pointers to the shell Interrupt
+    // Service Routines (ISR).
+    // This will populate the entire table, even if the interrupt
+    // is not used in this example.  This is useful for debug purposes.
+    // The shell ISR routines are found in F2806x_DefaultIsr.c.
+    // This function is found in F2806x_PieVect.c.
+    //
+    InitPieVectTable();
+    ///////////////////////////////////////////////////////////////////////
+    InitGpio();
+    InitCpuTimers();
+
+    InitAdc();
+    PieCtrlRegs.PIEIER1.bit.INTx1 = 1; // Enable INT 1.1 in the PIE
+    IER |= M_INT1;                     // Enable CPU Interrupt 1
+    ERTM;                              // Enable Global realtime interrupt DBGM
+    //使能全局中断
+    EINT;
 }
 
